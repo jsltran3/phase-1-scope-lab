9dfb8a5213ceea04137699cc853c06a2f79384f8
Joseph <jsltran@gmail.com>
2021-12-23 12:04:15 -0800

    commit tree f6627842f118cecf6c69dca03754445735d72826

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'; 
+const leastFavoriteCustomer = 'absolutely bob';
+
+function upperCaseCustomerName(thingie) {
+    customerName = 'BOB';
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'most likely bob';
+}
